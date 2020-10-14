fa52d4f8fd624d9f743edc4df4191530815d2cb2
Surabhi B <60255579+20171cse0691@users.noreply.github.com>
2020-10-14 14:13:05 +0530

    Update DataSource.php

--- DataSource.php
+++ DataSource.php
@@ -1,25 +1,12 @@
 <?php
-/**
- * Copyright (C) Phppot
- *
- * Distributed under 'The MIT License (MIT)'
- * In essense, you can do commercial use, modify, distribute and private use.
- * Though not mandatory, you are requested to attribute Phppot URL in your code or website.
- */
+
 namespace Phppot;
 
-/**
- * Generic datasource class for handling DB operations.
- * Uses MySqli and PreparedStatements.
- *
- * @version 2.7 - PDO connection option added
- */
+
 class DataSource
 {
 
-    // PHP 7.1.0 visibility modifiers are allowed for class constants.
-    // when using above 7.1.0, declare the below constants as private
-    // for better encapsulation
+    
     const HOST = 'localhost';
 
     const USERNAME = 'root';
@@ -30,27 +17,13 @@ class DataSource
 
     private $conn;
 
-    /**
-     * PHP implicitly takes care of cleanup for default connection types.
-     * So no need to worry about closing the connection.
-     *
-     * Singletons not required in PHP as there is no
-     * concept of shared memory.
-     * Every object lives only for a request.
-     *
-     * Keeping things simple and that works!
-     */
+   
     function __construct()
     {
         $this->conn = $this->getConnection();
     }
 
-    /**
-     * If connection object is needed use this method and get access to it.
-     * Otherwise, use the below methods for insert / update / etc.
-     *
-     * @return \mysqli
-     */
+   
     public function getConnection()
     {
         $conn = new \mysqli(self::HOST, self::USERNAME, self::PASSWORD, self::DATABASENAME);
@@ -63,11 +36,7 @@ class DataSource
         return $conn;
     }
 
-    /**
-     * If you wish to use PDO use this function to get a connection instance
-     *
-     * @return \PDO
-     */
+    
     public function getPdoConnection()
     {
         $conn = FALSE;
@@ -81,14 +50,7 @@ class DataSource
         return $conn;
     }
 
-    /**
-     * To get database results
-     *
-     * @param string $query
-     * @param string $paramType
-     * @param array $paramArray
-     * @return array
-     */
+   
     public function select($query, $paramType = "", $paramArray = array())
     {
         $stmt = $this->conn->prepare($query);
@@ -111,14 +73,7 @@ class DataSource
         }
     }
 
-    /**
-     * To insert
-     *
-     * @param string $query
-     * @param string $paramType
-     * @param array $paramArray
-     * @return int
-     */
+    
     public function insert($query, $paramType, $paramArray)
     {
         $stmt = $this->conn->prepare($query);
@@ -129,13 +84,7 @@ class DataSource
         return $insertId;
     }
 
-    /**
-     * To execute query
-     *
-     * @param string $query
-     * @param string $paramType
-     * @param array $paramArray
-     */
+   
     public function execute($query, $paramType = "", $paramArray = array())
     {
         $stmt = $this->conn->prepare($query);
@@ -146,15 +95,7 @@ class DataSource
         $stmt->execute();
     }
 
-    /**
-     * 1.
-     * Prepares parameter binding
-     * 2. Bind prameters to the sql statement
-     *
-     * @param string $stmt
-     * @param string $paramType
-     * @param array $paramArray
-     */
+    
     public function bindQueryParams($stmt, $paramType, $paramArray = array())
     {
         $paramValueReference[] = & $paramType;
@@ -167,14 +108,7 @@ class DataSource
         ), $paramValueReference);
     }
 
-    /**
-     * To get database results
-     *
-     * @param string $query
-     * @param string $paramType
-     * @param array $paramArray
-     * @return array
-     */
+    
     public function getRecordCount($query, $paramType = "", $paramArray = array())
     {
         $stmt = $this->conn->prepare($query);
